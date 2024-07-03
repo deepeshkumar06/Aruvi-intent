8c11e9576f95b59cfc2e2184cebedca492cff17f
deepeshkumar06 <deepeshkumar.p2004@gmail.com>
2024-07-03 13:05:45 +0530

    #

--- function.py
+++ function.py
@@ -10,13 +10,22 @@
 # bodmas(45,20)
 # bodmas(45,20,10)
 
-def student(name,deg,role):
-    print(f'My Name is {name}, I have completed {deg} & and working as the role {role}')
-student('Deepesh','B.E','Software Developer')
+# def student(name,deg,role):
+#     print(f'My Name is {name}, I have completed {deg} & and working as the role {role}')
+# student('Deepesh','B.E','Software Developer')
 
-def odd(a):
-    if a%2 == 0:
-        print('even')
-    else:
-        print('odd')
-odd(10)       
+# def odd(a):
+#     if a%2 == 0:
+#         print('even')
+#     else:
+#         print('odd')
+# a = int(input("enter : "))
+# odd(a)
+
+b = [1,2,3,4,5]
+
+def no(a):
+    print("Number is ",a)
+
+for i in range(len(b)):
+    no(b[i])
