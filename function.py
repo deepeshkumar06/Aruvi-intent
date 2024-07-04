c9386a67668d9259d48eb3e16377a431d6e3b25a
deepeshkumar06 <deepeshkumar.p2004@gmail.com>
2024-07-04 12:32:42 +0530

    function finished

--- function.py
+++ function.py
@@ -22,19 +22,50 @@
 # a = int(input("enter : "))
 # odd(a)
 
-b = [1,2,3,4,5]
+# b = [1,2,3,4,5]
 
-def no(a):
-    print("Number is ",a)
+# def no(a):
+#     print("Number is ",a)
 
-for i in range(len(b)):
-    no(b[i])
+# for i in range(len(b)):
+#     no(b[i])
 
-def add(a,b):
-    print(a+b)
+# def add(a,b):
+#     print(a+b)
 
-add(10,20)
-add(40,5)
+# add(10,20)
+# add(40,5)
 
-n=10
-c = n>8
+# n=10
+# c = n>8
+
+#Arbitary function:*,**:
+
+def lang(*a):
+    for l in a:
+        print("I Like to learn : ",l)
+lang("C","C++","Python")
+
+def stu(**karg):
+    for key,value in karg.items():
+        print(key,value)
+stu(Name="Deepesh",Degree="B.E",Role="Developer")
+
+def stu(**karg):
+    for key,value in karg.items():
+        print('%s == %s'%(key,value))
+stu(Name="Deepesh",Degree="B.E",Role="Developer")
+
+
+#Lambda Function
+
+a = lambda x,y,z : print('%s'%(x+y+z))
+a(4,5,6)
+
+li = [1,2,3,4,5,6]
+even = list(filter(lambda x : x%2==0,li))
+print(even)
+
+li = list(range(10))
+square = list(map(lambda s : s**2,li))
+print(square)
